3a27e6cccc39c53ffdfe2dd8f6260fd994e8f68c
derrickpryce <dpryce7@gmail.com>
2026-06-24 04:14:48 -0400

    Another commit.

--- src/main/java/io/homeinventory/menu/ReportMenu.java
+++ src/main/java/io/homeinventory/menu/ReportMenu.java
@@ -1,15 +1,53 @@
 package io.homeinventory.menu;
 
 import io.homeinventory.interfaces.IMenu;
+import io.homeinventory.service.ReportService;
+import io.homeinventory.util.InputHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ReportMenu implements IMenu {
+
+    @Autowired
+    private ReportService reportService;
+
     @Override
     public void show() {
+        int choice;
+        do {
+            printOptions();
+            choice = InputHandler.getIntegerInput();
+            switch (choice) {
+                case 1 -> homeSummary();
+                case 2 -> roomReport();
+                case 3 -> insuranceReport();
+                case 4 -> valueByCategory();
+            }
+        }
+        while (choice != 0);
+    }
 
+    public void valueByCategory() {
+    }
+
+    public void insuranceReport() {
+    }
+
+    public void roomReport() {
+    }
+
+    public void homeSummary() {
+        reportService.generateHomeSummary();
     }
 
     @Override
     public void printOptions() {
-
+        System.out.println("[1]  Home summary");
+        System.out.println("[2]  Room-by-room report");
+        System.out.println("[3]  Insurance report");
+        System.out.println("[4] Value by category");
+        System.out.println("[0] Back");
     }
+
 }
--- src/main/java/io/homeinventory/service/ReportService.java
+++ src/main/java/io/homeinventory/service/ReportService.java
@@ -1,9 +1,47 @@
 package io.homeinventory.service;
 
+import io.github.kusoroadeolu.clique.Clique;
+import io.github.kusoroadeolu.clique.components.Table;
+import io.github.kusoroadeolu.clique.configuration.TableType;
+import io.homeinventory.authentication.SessionContext;
+import io.homeinventory.model.Item;
+import io.homeinventory.repository.ItemRepository;
+import io.homeinventory.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportService {
-    // TODO: inject ItemRepository, RoomRepository
-    // generateHomeSummary(), generateRoomReport(), generateInsuranceReport(), getValueByCategory()
+    @Autowired
+    private ItemRepository itemRepository;
+  @Autowired
+    private RoomRepository roomRepository;
+
+    public void generateHomeSummary() {
+        int totalItems = itemRepository.findByUserId(SessionContext.getUser().getId()).size();
+        int totalRooms = roomRepository.findByUserId(SessionContext.getUser().getId()).size();
+        double totalEstimatedValue = itemRepository.findByUserId(SessionContext.getUser().getId()).stream().mapToDouble(Item::getEstimatedValue).sum();
+        double totalDeprecatedValue = itemRepository.findByUserId(SessionContext.getUser().getId()).stream().mapToDouble(Item::getEstimatedValue).sum();
+
+        Table table = Clique.table(TableType.BOX_DRAW)
+                .headers(
+                        "[*blue, bold]TOTAL ITEMS[/]",
+                        "[*blue, bold]TOTAL ROOMS[/]",
+                        "[*blue, bold]TOTAL ESTIMATED VALUE[/]",
+                        "[*blue, bold]TOTAL DEPRECATED VALUE[/]"
+                )
+                .row(String.valueOf(totalItems), String.valueOf(totalRooms), "$" + totalEstimatedValue, "$" + totalDeprecatedValue);
+    }
+
+    public void generateRoomReport() {
+
+    }
+
+    public void generateInsuranceReport() {
+
+    }
+
+    public void getValueByCategory() {
+
+    }
 }
